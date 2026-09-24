4ce74d8c130b6f7320880f906b1a2d1aae2fc931
Bhavana Marpadaga <marpadagabhavana@gmail.com>
2026-09-24 09:18:54 -0500

    Rotate Image Matrix

--- Rotate_Image.c
+++ Rotate_Image.c
@@ -32,19 +32,21 @@ Hint: A common approach is to first transpose the matrix and then reverse each r
 #include <stdlib.h>
 
 void rotate(int** matrix, int matrixSize, int* matrixColSize){
-    int** m = malloc(matrixSize * sizeof(int*));
-    for (int i=0; i< *matrixColSize; i++){
-        m[i] = malloc(matrixSize * sizeof(int));
-        for(int j=0; j<matrixSize; j++){
-            m[j][i] = matrix[i][j];
-
+    int n = matrixSize;
+    // transpose
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++) {
+            int t = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = t;
         }
-
-    }
-    matrix = m;
-
-
-
+    // reverse each row
+    for (int i = 0; i < n; i++)
+        for (int l = 0, r = n - 1; l < r; l++, r--) {
+            int t = matrix[i][l];
+            matrix[i][l] = matrix[i][r];
+            matrix[i][r] = t;
+        }
 }
 
 void free_matrix(int** m, int n) {
